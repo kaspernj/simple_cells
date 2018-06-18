64a953d4f6683ef5c2c88dc58e0e8ece31ae4644
kjabtion <kj@abtion.com>
2018-06-18 09:26:31 +0000

    Removed StringCases usage and replaced with Rails helpers

--- app/helpers/simple_cells/views_helper.rb
+++ app/helpers/simple_cells/views_helper.rb
@@ -1,9 +1,8 @@
 module SimpleCells::ViewsHelper
   def render_simple_cell(cell_name, action_name, simple_cell_arguments = {})
-    cell_class_name = StringCases.snake_to_camel(cell_name)
-    cell_class_name << "SimpleCell"
+    cell_class_name = "#{cell_name.camelize}SimpleCell"
 
-    cell_instance = StringCases.constantize(cell_class_name).new(view_context: self, simple_cell_arguments: simple_cell_arguments)
+    cell_instance = cell_class_name.constantize.new(view_context: self, simple_cell_arguments: simple_cell_arguments)
     cell_instance.__send__(action_name)
     cell_instance.render_simple_cell_view(cell_name, action_name)
   end
