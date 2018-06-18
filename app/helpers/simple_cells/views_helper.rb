4657bb46c31c60a18b86d687f33bb1b5746a98b3
kaspernj <kaspernj@gmail.com>
2018-06-18 09:33:02 +0000

    Upgraded gems and fixed issue

--- app/helpers/simple_cells/views_helper.rb
+++ app/helpers/simple_cells/views_helper.rb
@@ -1,6 +1,6 @@
 module SimpleCells::ViewsHelper
   def render_simple_cell(cell_name, action_name, simple_cell_arguments = {})
-    cell_class_name = "#{cell_name.camelize}SimpleCell"
+    cell_class_name = "#{cell_name.to_s.camelize}SimpleCell"
 
     cell_instance = cell_class_name.constantize.new(view_context: self, simple_cell_arguments: simple_cell_arguments)
     cell_instance.__send__(action_name)
--- simple_cells.gemspec
+++ simple_cells.gemspec
@@ -16,7 +16,6 @@ Gem::Specification.new do |s|
 
   s.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
 
-  s.add_dependency "rails", ">= 4.0.0"
+  s.add_dependency "rails", ">= 4.0.0", "< 5.0.0"
   s.add_dependency "auto_autoloader", ">= 0.0.4"
-  s.add_dependency "string-cases", ">= 0.0.4"
 end
